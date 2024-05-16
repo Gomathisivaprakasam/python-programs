79f16c6d2a6cf0ed9db0adf803b1ae5aecc3551e
Gomathisivaprakasam <gomathi95kannan@gmail.com>
2024-05-16 13:08:57 +0530

    Delete 35.py

--- 35.py
+++
@@ -1,8 +0,0 @@
-digit = [str(x)for x in range(10)]
-mystr = 'HE234LLO,PY948TH23O'
-chars = []
-for x in  mystr:
-    if x not in digit:
-        chars .append(x)
-newstr = ''.join(chars)
-print(newstr)
