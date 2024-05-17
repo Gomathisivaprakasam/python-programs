25ee8c56ef33b35701d62a35bfd5f4daed02fdcf
Gomathisivaprakasam <gomathi95kannan@gmail.com>
2024-05-17 09:09:53 +0530

    Add files via upload

---
+++ 09may24/1.py
@@ -0,0 +1,9 @@
+mark=int(input))
+result=" "
+if mark<30:
+    :wq
+    result="failed"
+elif:
+mark>30:
+result:"passed"
+print(result)
---
+++ 09may24/3.py
@@ -0,0 +1,3 @@
+num={10:"Ten",20:"Twent",30:"thirty"}
+for x in num:
+    print(x)
